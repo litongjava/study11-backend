18d07c03cd61ebfd2aba4bb40aa7d74812a8c4ad
Tong Li <litongjava001@gmail.com>
2025-09-08 22:16:03 -1000

    update PlatformAndModelSetService

---
+++ study11-business/src/main/java/com/litongjava/study11/consts/Study11DomainConst.java
@@ -0,0 +1,5 @@
+package com.litongjava.study11.consts;
+
+public interface Study11DomainConst {
+  String PREVIEW_STUDY11_AI = "preview.study11.ai";
+}
--- study11-business/src/main/java/com/litongjava/study11/service/PlatformAndModelSetService.java
+++ study11-business/src/main/java/com/litongjava/study11/service/PlatformAndModelSetService.java
@@ -1,13 +1,17 @@
 package com.litongjava.study11.service;
 
+import com.litongjava.bailian.BaiLianAiModels;
 import com.litongjava.cerebras.CerebrasModels;
 import com.litongjava.chat.UniChatRequest;
 import com.litongjava.consts.ModelPlatformName;
+import com.litongjava.study11.consts.Study11DomainConst;
 
 public class PlatformAndModelSetService {
 
   public void configPlatformAndModel(UniChatRequest uniChatRequest) {
-//    uniChatRequest.setPlatform(ModelPlatformName.BAILIAN).setModel(BaiLianAiModels.QWEN3_CODER_PLUS);
-    uniChatRequest.setPlatform(ModelPlatformName.CEREBRAS).setModel(CerebrasModels.QWEN_3_CODER_480B);
+    if (Study11DomainConst.PREVIEW_STUDY11_AI.equals(uniChatRequest.getDomain())) {
+      uniChatRequest.setPlatform(ModelPlatformName.CEREBRAS).setModel(CerebrasModels.QWEN_3_CODER_480B);
+    }
+    uniChatRequest.setPlatform(ModelPlatformName.BAILIAN).setModel(BaiLianAiModels.QWEN3_CODER_PLUS);
   }
 }
