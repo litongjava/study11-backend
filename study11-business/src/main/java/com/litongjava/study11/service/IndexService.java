2ecdf1c3705067ec0e6822cbbb0ed6b1f7b20eff
Tong Li <litongjava001@gmail.com>
2025-09-21 22:45:38 -1000

    update tio-boot-admin version

--- study11-business/src/main/java/com/litongjava/study11/service/IndexService.java
+++ study11-business/src/main/java/com/litongjava/study11/service/IndexService.java
@@ -5,14 +5,14 @@ import java.util.List;
 import com.jfinal.kit.Kv;
 import com.litongjava.db.activerecord.Db;
 import com.litongjava.db.activerecord.Row;
-import com.litongjava.template.EnjoyTemplate;
+import com.litongjava.template.EnjoyEngine;
 
 public class IndexService {
 
   public String index() {
     List<Row> data = Db.find("select id,topic,language,view_count,create_time from study11_html_code order by id");
     Kv kv = Kv.by("data", data);
-    String html = EnjoyTemplate.renderToString("index.html", kv);
+    String html = EnjoyEngine.renderToString("index.html", kv);
     return html;
   }
 }
